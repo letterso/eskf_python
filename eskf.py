ff6cea50cf8f472c7e4d6727a5145b92b39f62b2
letterso <lettersony00@gmail.com>
2024-02-22 17:37:27 +0800

    初步提交(未完成)

---
+++ eskf.py
@@ -0,0 +1,273 @@
+import numpy as np
+import collections
+from liegroups.liegroups.numpy import SO3, SE3
+
+
+class IMUState():
+    """ IMU数据
+    """
+
+    def __init__(self, timestamp: float, gyro: np.array, acce: np.array) -> None:
+        self.timestamp_ = timestamp
+        self.gyro_ = gyro
+        self.acce_ = acce
+
+
+class StaticIMUInit():
+    def __init__(self, logger):
+        """ 系统初始化
+        """
+        # 日志
+        self.logger = logger
+
+        # 初始化判断标志位
+        self.init_success = False
+
+        # 数据缓存
+        self.imu_deque_ = collections.deque()
+
+        # 参数
+        self.init_time_seconds_ = 2             # 初始化需要采集时间
+        self.init_imu_queue_max_size_ = 2000    # IMU数据缓存
+        self.max_static_gyro_var_ = 0.5        # 静态下陀螺测量方差
+        self.max_static_acce_var_ = 0.05       # 静态下加计测量方差
+        self.gravity_norm_ = 9.81              # 重力大小
+
+        # 零偏和噪声
+        self.gravity_ = np.zeros(3)
+        self.init_bg_ = 0.0
+        self.init_ba_ = 0.0
+        self.gyro_cov_ = np.zeros(3)
+        self.acce_cov_ = np.zeros(3)
+
+    def AddIMU(self, imu_data: IMUState):
+        self.imu_deque_.append(imu_data)
+        if len(self.imu_deque_) > self.init_imu_queue_max_size_:
+            self.imu_deque_.popleft()
+
+        init_time = imu_data.timestamp_ - self.imu_deque_[0].timestamp_
+        if init_time > self.init_time_seconds_:
+            self.TryInit()
+
+    def TryInit(self):
+        """ 进行初始化操作
+        """
+
+        # 获取原始数据
+        imu_gyro_raw = np.zeros([len(self.imu_deque_), 3])
+        imu_acce_raw = np.zeros([len(self.imu_deque_), 3])
+        count = 0
+        for imu_data in self.imu_deque_:
+            imu_gyro_raw[count] = imu_data.gyro_
+            imu_acce_raw[count] = imu_data.acce_
+            count += 1
+
+        # 计算均值和方差
+        gyro_mean = np.mean(imu_gyro_raw, axis=0)
+        self.gyro_cov_ = np.var(imu_gyro_raw, axis=0)
+
+        acce_mean = np.mean(imu_acce_raw, axis=0)
+        self.gravity_ = -acce_mean / \
+            np.linalg.norm(acce_mean) * self.gravity_norm_
+        imu_acce = imu_acce_raw+self.gravity_
+        acce_mean = np.mean(imu_acce, axis=0)
+        self.acce_cov_ = np.var(imu_acce, axis=0)
+
+        # 检查IMU噪声
+        if np.linalg.norm(gyro_mean) > self.max_static_gyro_var_:
+            self.logger.warning("gyro noise too big {}".format(
+                np.linalg.norm(gyro_mean)))
+            self.init_success = False
+            return
+
+        if np.linalg.norm(acce_mean) > self.max_static_acce_var_:
+            self.logger.warning("acce noise too big {}".format(
+                np.linalg.norm(acce_mean)))
+            self.init_success = False
+            return
+
+        # 估计测量噪声和零偏
+        self.init_bg_ = gyro_mean
+        self.init_ba_ = acce_mean
+        self.logger.info("IMU init successful")
+        self.logger.info("bg = {}, gyro sq = {}".format(
+            self.init_bg_, self.gyro_cov_))
+        self.logger.info("ba = {}, acce sq = {}".format(
+            self.init_ba_, self.acce_cov_))
+        self.logger.info("grav = {}, norm = {}".format(
+            self.gravity_, np.linalg.norm(self.gravity_)))
+        self.init_success = True
+
+    def InitSuccess(self) -> bool:
+        return self.init_success
+
+
+class ESKF:
+    def __init__(self) -> None:
+        # 历史信息
+        self.last_imu_ = IMUState(0, np.array([0, 0, 0]), np.array([0, 0, 0]))
+
+        # State: p, v, q, ba, bg, g
+        # 名义状态
+        self.P_ = np.zeros(3).reshape(-1, 1)
+        self.v_ = np.zeros(3).reshape(-1, 1)
+        self.R_ = SO3(np.identity(3))
+        self.ba_ = np.zeros(3).reshape(-1, 1)
+        self.bg_ = np.zeros(3).reshape(-1, 1)
+        self.g_ = np.array([[0], [0], [-9.8]])
+
+        # 误差状态
+        self.dx_ = np.zeros(18).reshape(-1, 1)
+
+        # 协方差阵
+        self.cov_ = np.identity(18)
+
+        # 观测参数
+        self.gnss_pos_noise_ = 0.1
+        self.gnss_height_noise_ = 0.1
+        self.gnss_ang_noise_ = np.deg2rad(1.0)
+
+        self.options_ = self.Options()
+
+    class Options():
+        def __init__(self) -> None:
+            # IMU参数
+            self.imu_dt_ = 0.01         # 频率
+            self.gyro_var_ = 1e-5       # 磁力计噪声
+            self.acce_var_ = 1e-2       # 加速计噪声
+            self.bias_gyro_var_ = 1e-2  # 磁力计零飘
+            self.bias_acce_var_ = 1e-2  # 加速计零飘
+
+            # 更新参数
+            self.update_bias_gyro_ = True
+            self.update_bias_acce_ = True
+
+    def GetNominalState(self):
+        return self.last_imu_, self.P_, self.v_, self.R_, self.ba_, self.bg_
+
+    def SetInitialConditions(self, options: Options, init_ba: float, init_bg: float, gravity: float):
+        # 构建噪声
+        self.BuildProcessNoise(options)
+        self.options_ = options
+
+        # 更新初始状态
+        self.bg_ = init_bg.reshape(-1, 1)
+        self.ba_ = init_ba.reshape(-1, 1)
+        self.g_ = gravity.reshape(-1, 1)
+        self.cov_ = np.identity(18) * 1e-4
+
+    def BuildProcessNoise(self, options: Options):
+        # 过程噪声
+        # [P, V, R, Ba, Bg, g]
+        ev2 = options.acce_var_  # * options.acce_var_
+        et2 = options.gyro_var_  # * options.gyro_var_
+        eg2 = options.bias_gyro_var_  # * options.bias_gyro_var_
+        ea2 = options.bias_acce_var_  # * options.bias_acce_var_
+
+        self.Q_ = np.diag([0, 0, 0,
+                           ev2, ev2, ev2,
+                           et2, et2, et2,
+                           eg2, eg2, eg2,
+                           ea2, ea2, ea2,
+                           0, 0, 0])
+
+    def BuildMeasurementNoise(self, options: Options):
+        # 测量噪声
+        # [P,R]
+        gp2 = self.gnss_pos_noise_ * self.gnss_pos_noise_
+        gh2 = self.gnss_height_noise_ * self.gnss_height_noise_
+        ga2 = self.gnss_ang_noise_ * self.gnss_ang_noise_
+        self.gnss_noise_ = np.diag([gp2, gp2, gh2,
+                                    ga2, ga2, ga2])
+
+    def Predict(self, imu: IMUState) -> bool:
+        # 检查数据
+        dt = imu.timestamp_ - self.last_imu_.timestamp_
+        if dt > 5 * self.options_.imu_dt_ or dt < 0:
+            self.last_imu_ = imu
+            return False
+
+        # 名义递推,(3.41)
+        new_p = self.P_ + self.v_ * dt + 0.5 * \
+            (self.R_.as_matrix() @ (imu.acce_.reshape(-1, 1) - self.ba_)) * \
+            dt*dt + 0.5 * self.g_ * dt * dt
+        new_v = self.v_ + self.R_.as_matrix() @ (imu.acce_.reshape(-1, 1) -
+                                                 self.ba_) * dt + self.g_ * dt
+        new_R = self.R_.dot(
+            SO3.exp(((imu.gyro_.reshape(-1, 1) - self.bg_)*dt).reshape(-1)))
+
+        # 更新名义状态，ba,bg,g不变
+        self.P_ = new_p
+        self.v_ = new_v
+        self.R_ = new_R
+
+        # 误差递推，(3.47)
+        # 计算运动过程雅可比矩阵 F
+        F = np.identity(18)
+        F[0:3, 3:6] = np.identity(3) @ np.array([[dt], [dt], [dt]])
+        F[3:6, 6:9] = -1 * self.R_.as_matrix() @ SO3.wedge((imu.acce_.reshape(-1, 1) -
+                                                            self.ba_).reshape(-1)) @ np.array([[dt], [dt], [dt]])
+        F[3:6, 12:15] = -1 * self.R_.as_matrix() @ np.array([[dt], [dt], [dt]])
+        F[3:6, 15:18] = np.identity(3) @ np.array([[dt], [dt], [dt]])
+        F[6:9, 6:9] = SO3.exp(
+            (-1*(imu.gyro_.reshape(-1, 1)-self.bg_)*dt).reshape(-1)).as_matrix()
+        F[6:9, 9:12] = -np.identity(3) @ np.array([[dt], [dt], [dt]])
+
+        # 更新协方差矩阵，(3.48)
+        self.dx_ = F @ self.dx_
+        self.cov_ = F @ self.cov_ @ F.T + self.Q_
+        self.last_imu_ = imu
+        return True
+
+    def UpdateAndReset(self):
+        # 名义变量+误差变量
+        self.P_ = self.P_ + self.dx_[0:3, 0:1]
+        self.v_ = self.v_ + self.dx_[3:6, 0:1]
+        self.R_ = SO3(self.R_.as_matrix() @
+                      SO3.exp(self.dx_[6:9, 0:1].reshape(-1)).as_matrix())
+        if self.options_.update_bias_gyro_:
+            self.bg_ = self.bg_ + self.dx_[9:12, 0:1]
+        if self.options_.update_bias_acce_:
+            self.ba_ = self.ba_ + self.dx_[12:15, 0:1]
+        self.g_ = self.g_ + self.dx_[15:18, 0:1]
+
+        # P阵进行投影，(3.63)
+        J = np.identity(18)
+        J[6:9, 6:9] = np.identity(
+            3) - SO3.wedge(self.dx_[6:9, 0:1].reshape(-1))*0.5
+        self.cov_ = J @ self.cov_ @ J.T
+
+        # 清空误差变量
+        self.dx_ = np.zeros(18).reshape(-1, 1)
+
+    def ObserveSE3(self, pose_tran: np.array, pose_SO3: np.array, trans_noise: float, ang_noise: float) -> bool:
+        """SE3观测
+
+        Args:
+            pose_tran (np.array): 观测平移
+            pose_SO3 (np.array): 观测平移旋转
+            trans_noise (float): 平移噪声
+            ang_noise (float): 旋转噪声
+
+        Returns:
+            bool: _description_
+        """
+        # 观测状态变量中的p, R，H为6x18，其余为零
+        H = np.zeros([6, 18])
+        H[0:3, 0:3] = np.identity(3)
+        H[3:6, 6:9] = np.identity(3)
+
+        # 卡尔曼增益和更新过程
+        noise_cov = np.diag(
+            [trans_noise, trans_noise, trans_noise, ang_noise, ang_noise, ang_noise])
+        K = self.cov_ @ H.T @ np.linalg.inv(H @ self.cov_ @  H.T + noise_cov)
+
+        # 更新x和cov
+        innov = np.zeros(6).reshape(-1, 1)
+        innov[0:3, 0:1] = pose_tran - self.P_
+        innov[3:6, 0:1] = SO3(self.R_.inv().as_matrix() @
+                              pose_SO3).log().reshape(-1, 1)
+        self.dx_ = K @ innov
+        self.cov_ = (np.identity(18) - K @ H) @ self.cov_
+        self.UpdateAndReset()
+        return True
---
+++ run_eskf_gins.py
@@ -0,0 +1,172 @@
+from pathlib import Path
+import logging
+import colorlog
+import math
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import utm
+from eskf import ESKF, StaticIMUInit, IMUState
+
+
+def ConvertGps2UTM(gps_msg, antenna_pos, antenna_angle):
+    # pos
+    utm_pos = np.zeros(3)
+    utm_pos[0], utm_pos[1], _, _ = utm.from_latlon(
+        gps_msg[0], gps_msg[1])
+    utm_pos[2] = gps_msg[2]
+    # heading
+    utm_angle = np.deg2rad(90 - gps_msg[3])
+
+    # TWG 转到 TWB
+    TGB = np.identity(4)
+    TGB[0:3, 0:3] = np.array([[np.cos(antenna_angle), -np.sin(antenna_angle), 0],
+                              [np.sin(antenna_angle),  np.cos(
+                                  antenna_angle), 0],
+                              [0,           0, 1]]).T
+    TGB[0:3, 3:4] = -TGB[0:3, 0:3] @ np.array([[antenna_pos[0]], [antenna_pos[1]], [0]])
+    TWG = np.identity(4)
+    TWG[0:3, 0:3] = np.array([[np.cos(utm_angle), -np.sin(utm_angle), 0],
+                              [np.sin(utm_angle),  np.cos(utm_angle), 0],
+                              [0,           0, 1]])
+    TWG[0:3, 3:4] = np.array([[utm_pos[0]], [utm_pos[1]], [utm_pos[2]]])
+    TWB = TWG @ TGB
+
+    return TWB[0:3, 3:4], TWB[0:3, 0:3]
+
+
+if __name__ == '__main__':
+    # RTK天线安装偏移
+    antenna_angle = np.deg2rad(12.06)
+    antenna_pox_x = -0.17
+    antenna_pox_y = -0.20
+    first_gps = True
+    origin_pose = np.zeros(3).reshape(-1, 1)
+
+    # 图形界面
+    with_ui = True
+    h_predict_p = np.zeros((3, 1))
+    h_gnss_p = np.zeros((3, 1))
+    h_update_p = np.zeros((3, 1))
+
+    # 日志
+    # 创建logger对象
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    # 创建控制台日志处理器
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    color_formatter = colorlog.ColoredFormatter(
+        '%(log_color)s%(levelname)s: %(message)s',
+        log_colors={
+            'DEBUG': 'cyan',
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'red,bg_white',
+        }
+    )
+    console_handler.setFormatter(color_formatter)
+    # 创建文件日志处理器
+    file_handler = logging.FileHandler(
+        str(Path.cwd() / 'output'/'log.txt'), mode="a", encoding="utf-8")
+    file_handler.setFormatter(logging.Formatter("%(levelname)s: %(message)s"))
+    file_handler.setLevel(logging.WARN)
+    # 移除默认的handler
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+    # 将日志处理器添加到logger对象
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    # ESKF
+    eskf_filter = ESKF()
+    eskf_init = StaticIMUInit(logger)
+    eskf_init_finish = False
+
+    with (Path.cwd() / 'data'/'10.txt').open('r') as file:
+        for line in file:
+            # 数据获取
+            line = line.strip()  # 去除行末的换行符和空白字符
+            data_items = line.split()  # 使用空格分隔数据项
+
+            if not eskf_init_finish:
+                # 初始化
+                if not eskf_init.InitSuccess():
+                    if data_items[0] == 'IMU':
+                        gyro = np.array([float(data_items[2]), float(
+                            data_items[3]), float(data_items[4])])
+                        acce = np.array([float(data_items[5]), float(
+                            data_items[6]), float(data_items[7])])
+                        imu_data = IMUState(float(data_items[1]), gyro, acce)
+                        eskf_init.AddIMU(imu_data)
+
+                if eskf_init.InitSuccess():
+                    options = ESKF.Options()
+                    options.gyro_var_ = math.sqrt(eskf_init.gyro_cov_[0])
+                    options.acce_var_ = math.sqrt(eskf_init.acce_cov_[0])
+                    eskf_filter.SetInitialConditions(
+                        options, eskf_init.init_ba_, eskf_init.init_bg_, eskf_init.gravity_)
+                    eskf_init_finish = True
+
+            else:
+                # 预测
+                if data_items[0] == 'IMU':
+                    gyro = np.array([float(data_items[2]), float(
+                        data_items[3]), float(data_items[4])])
+                    acce = np.array([float(data_items[5]), float(
+                        data_items[6]), float(data_items[7])])
+                    imu_data = IMUState(float(data_items[1]), gyro, acce)
+                    eskf_filter.Predict(imu_data)
+                    _, pred_P, pred_v, pred_R, _, _ = eskf_filter.GetNominalState()
+                    h_predict_p = np.hstack((h_predict_p, pred_P))
+                    logger.debug("predict:\n p: {} \n v: {} \n r: {}".format(
+                        pred_P, pred_v, pred_R))
+
+                # 更新
+                if data_items[0] == 'GNSS':
+
+                    gnss_pos, gnss_r = ConvertGps2UTM(np.array([float(data_items[2]), float(data_items[3]), float(data_items[4]), float(data_items[5])]),
+                                                            np.array([antenna_pox_x, antenna_pox_y]),
+                                                            antenna_angle)  # lat lon alt heading
+
+                    if first_gps:
+                        origin_pose = gnss_pos
+                        first_gps = False
+                        continue
+
+                    # 移除起点
+                    gnss_pos = gnss_pos - origin_pose
+                    h_gnss_p = np.hstack((h_gnss_p, gnss_pos))
+
+                    trans_noise = 0.1
+                    ang_noise = np.deg2rad(1.0)
+                    eskf_filter.ObserveSE3(
+                        gnss_pos, gnss_r, trans_noise, ang_noise)
+                    _, update_P, update_v, update_R, _, _ = eskf_filter.GetNominalState()
+                    logger.debug("update:\n p: {} \n v: {} \n r: {}".format(
+                        update_P, update_v, update_R))
+                    h_update_p = np.hstack((h_update_p, update_P))
+
+                if with_ui:
+                    plt.cla()
+                    # for stopping simulation with the esc key.
+                    # plt.gcf().canvas.mpl_connect('key_release_event',
+                    #         lambda event: [exit(0) if event.key == 'escape' else None])
+                    plt.plot(h_gnss_p[0, :], h_gnss_p[1, :], ".r")
+                    # plt.plot(h_update_p[0, :], h_update_p[1, :], ".g")
+                    plt.plot(h_predict_p[0, :], h_predict_p[1, :], ".b")
+                    # plt.plot(hxTrue[0, :].flatten(),
+                    #         hxTrue[1, :].flatten(), "-b")
+                    # plt.plot(hxDR[0, :].flatten(),
+                    #         hxDR[1, :].flatten(), "-k")
+                    # plt.plot(hxEst[0, :].flatten(),
+                    #         hxEst[1, :].flatten(), "-r")
+                    # plot_covariance_ellipse(xEst, PEst)
+                    plt.axis("equal")
+                    plt.grid(True)
+                    plt.pause(0.001)
+
+                time.sleep(0.01)
